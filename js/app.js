90eb8d5ac21e3384ec957fee300d05ea0c38faee
hagarsaleh <hagar567@gmail.com>
2020-07-21 20:26:57 +0200

    added javascript

--- js/app.js
+++ js/app.js
@@ -1,34 +1,19 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
-*/
 
 var ma = document.getElementsByTagName('main');
 var nav= document.getElementById("navbar__list");
 var i =0;
-// make list bel divs in main
 var list = document.getElementsByTagName('section');
-
-//var list = ma.querySelectorAll('div');
-//create li elements for each div in main
+const fragment = document.createDocumentFragment();
 function add(){
   do  {
-    var sec = document.getElementById(`section${i+1}`);
-    var li=  document.createElement('li');
-    var id = `section ${i+1}`;
-    li.setAttribute('id' ,`${id}`);
-    var text =document.createTextNode(id);
-    li.appendChild(text);
+    var li= document.createElement('li');
+    var a=  document.createElement('a');
+    var text =document.createTextNode(`section${i+1}`);
+    a.setAttribute('href' ,`#section${i+1}`);
+    a.setAttribute('target', '_self');
+    li.appendChild(a);
+    a.appendChild(text);
+    li.appendChild(a);
     nav.appendChild(li);
 
     i++;
@@ -38,85 +23,59 @@ function add(){
 }
 add();
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 
-// check if div in viewport
+// var nav= document.getElementById()
+// check if scrolling
+
+var timer = null;
+window.addEventListener('scroll', function() {
+    if(timer !== null) {
+        clearTimeout(timer);
+        nav.style.display='none';
+    }
+    timer = setTimeout(function()
+    {nav.style.display='block'}, 700);
+}, false);
 
 let options = {
 //  root: divs,
   rootMargin: '0px',
-  threshold: 1
+  threshold: .7
 }
-let n = 0;
 
+let n = 0;
 
 for(item of list){
 let item= document.getElementsByTagName("section")[n];
 let callback = (entries, observer) => {
-  entries.forEach(entry => {
-    item.setAttribute('class', 'your-active-class');
-    console.log(item.id);
-
-    // Each entry describes an intersection change for one observed
+    entries.forEach(entry => {
+  //  list.classList.toggle("your-active-class");
+  function myFunction(item) {
+if (document.querySelector('.your-active-class') !== null) {
+  document.querySelector('.your-active-class').classList.remove('your-active-class');
+}
+item.setAttribute('class', 'your-active-class');
+}
+    myFunction(item);
+    //console.log(item.id);
 
   });
 
 };
 let observer = new IntersectionObserver(callback, options);
-  observer.observe(list[n]);
+  observer.observe(item);
   n++;
 }
 
 
-
-//scroll lel section: using html get l location, we function onclick
-// how to loop to check which div in viewport? hoa when not scrolling
-///// check l link lw feeh l id bta3 l section ,,,train station use observer
-//where to get l ul name from ?,, class name ? name attribute azon
-//make class active ,style it we use it when in isElementInViewport
-
-
-
-//general, observer api helw lel performance ,,,smooth scroll using css
-//download l project files
-
-/**
- * Define Global Variables
- *
-*/
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- *
-*/
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
+console.log(performance.now());
